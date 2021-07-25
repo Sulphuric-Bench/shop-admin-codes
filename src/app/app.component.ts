c71421d50d2e4e7d5efe19df70796a37352f6d06
Sulphuric-Bench <sulphuricpanel@gmail.com>
2021-07-26 03:17:39 +0600

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'SBSadmin';
+  data: any;
+  approve(id: any) {
+    this.db.collection('waitingForApprovement').doc(id).update({
+      approved: true,
+    });
+  }
+  decline(id: any) {
+    this.db.collection('waitingForApprovement').doc(id).delete();
+  }
+  constructor(public db: AngularFirestore) {
+    db.collection('waitingForApprovement')
+      .snapshotChanges()
+      .subscribe((res) => (this.data = res));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBR36piXKbbiot9fL4YdH6s1ge3ewnXyt0',
+  authDomain: 'sulphur-shop.firebaseapp.com',
+  projectId: 'sulphur-shop',
+  storageBucket: 'sulphur-shop.appspot.com',
+  messagingSenderId: '333335396754',
+  appId: '1:333335396754:web:15540c122cbcf82fc1414b',
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
